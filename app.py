1090bde50fb2a70e488a517ec4007e473201ac3c
Amityemul <52160216+Amityemul@users.noreply.github.com>
2022-11-17 13:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,6 +167,6 @@ def reset_token(token):
 
 def main(event,context):
     app.run(debug=True)
-    print("9 changes for testing")
+    print("1 changes for testing")
 if __name__ == '__main__':  
    main()
